abfde38cf73eba1079626eff1f4f5faee0977a7c
Andy Jones <andy.jones@digital.education.gov.uk>
2024-04-24 20:53:23 +0100

    fixes

--- app/assets/js/feedback.js
+++ app/assets/js/feedback.js
@@ -1,30 +1,9 @@
-var feedbackTab = document.getElementById('feedback-tab');
-var feedbackPanel = document.getElementById('feedback-panel');
 
 var servicesTab = document.getElementById('services-tab');
 var servicesPanel = document.getElementById('services-panel');
 
-var feedbackForm = document.getElementById('feedback-form');
-var feedbackFormInput = document.getElementById('feedback_form_input');
-
-var submitButton = document.getElementById('submitbutton');
-var cancelButton = document.getElementById('cancelbutton');
-
 var closeNav = document.getElementById('closeNav');
 
-
-feedbackTab.addEventListener('click', function (event) {
-  event.preventDefault(); // prevent the default link behavior
-  if (feedbackPanel.style.display === 'none') {
-    feedbackPanel.style.display = 'block';
-    feedbackPanel.setAttribute('aria-hidden', 'false');
-    feedbackTab.setAttribute('aria-expanded', 'true');
-  } else {
-    feedbackPanel.style.display = 'none';
-    feedbackPanel.setAttribute('aria-hidden', 'true');
-    feedbackTab.setAttribute('aria-expanded', 'false');
-  }
-});
 
 servicesTab.addEventListener('click', function (event) {
   event.preventDefault(); // prevent the default link behavior
@@ -39,34 +18,19 @@ servicesTab.addEventListener('click', function (event) {
   }
 });
 
+document.addEventListener('keydown', function (event) {
+  // Check if the Escape key is pressed
+  if (event.key === "Escape" || event.key === "Esc") {
+    // Prevent the default action to avoid any side effects
+    event.preventDefault();
 
-submitButton.addEventListener('click', function (event) {
-  event.preventDefault();
-  var formData = new FormData(feedbackForm);
-  var xhr = new XMLHttpRequest();
-
-  xhr.open('POST', '/submit-feedback');
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      feedbackFormInput.value = "";
-      feedbackTab.textContent = 'Feedback submitted';
-      feedbackPanel.style.display = 'none';
-      feedbackPanel.setAttribute('aria-hidden', 'true');
-      feedbackTab.setAttribute('aria-expanded', 'false');
+    // Check if the servicesPanel is currently displayed
+    if (servicesPanel.style.display === 'block') {
+      servicesPanel.style.display = 'none';
+      servicesPanel.setAttribute('aria-hidden', 'true');
+      servicesTab.setAttribute('aria-expanded', 'false');
     }
-  };
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(new URLSearchParams(formData).toString());
-
-});
-
-cancelButton.addEventListener('click', function (event) {
-  event.preventDefault();
-
-  feedbackFormInput.value = "";
-  feedbackPanel.style.display = 'none';
-  feedbackPanel.setAttribute('aria-hidden', 'true');
-  feedbackTab.setAttribute('aria-expanded', 'false');
+  }
 });
 
 closeNav.addEventListener('click', function (event) {
